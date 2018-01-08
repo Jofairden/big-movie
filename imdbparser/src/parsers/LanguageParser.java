28ae38e91cbf1049e479eaa3378084d29eb306c1
Jofairden <danielzondervan.nl@gmail.com>
2018-01-09 00:58:28 +0100

    languages parse final fix

--- imdbparser/src/parsers/LanguageParser.java
+++ imdbparser/src/parsers/LanguageParser.java
@@ -1,35 +1,40 @@
 package parsers;
 
+import main.ImdbUtils;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /*
-	Authors: Fadi
-	@todo: parsed year still needs separation between year and roman numerals, roman numeral = movie with the same name released in the same year
+	Authors: Fadi, Daniël
  */
 // Will parse to the following: title;year;language;occurrence
 public final class LanguageParser extends Parser {
-
+	
 	private final Pattern seriesPatternMovies = Pattern.compile("(^\".+)");
 	private final Pattern moviesPatternRunningTimes = Pattern.compile("(.*)\\((\\d{4}|\\?{1,})(\\/)?(\\w*)?(\\))(\\s*)(\\(V\\))?\\s*((\\{\\{.*\\}\\})|\\(TV\\))?\\s*(.*)");
-
+	
 	private int header;
-
+	
 	public LanguageParser(String inputFile) {
 		super(inputFile);
 	}
-
+	
 	@Override
 	public boolean canParse(String line) {
 		return ++header > 14;
 	}
-
+	
 	@Override
 	public void parseLine(String line) {
 		Matcher seriesMatcher = seriesPatternMovies.matcher(line);
 		Matcher moviesMatcher = moviesPatternRunningTimes.matcher(line);
-
+		
 		if (!seriesMatcher.matches() && moviesMatcher.matches())
-			super.writeLine = moviesMatcher.replaceAll("$1;$2$4;$10 \n");
+			super.writeLine =
+					String.format("%s;%s;%s\n",
+							moviesMatcher.replaceAll("$1").trim(),
+							moviesMatcher.replaceAll("$2;$10"),
+							ImdbUtils.romanToDecimal(moviesMatcher.replaceAll("$4")));
 	}
 }
