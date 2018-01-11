e2e3a0fc2bf9ea8298e4dc416b9fab40585ef443
Jofairden <danielzondervan.nl@gmail.com>
2018-01-11 17:52:34 +0100

    update sql accordingly

--- imdbparser/sql/create_actor_movie_temp.sql
+++ imdbparser/sql/create_actor_movie_temp.sql
@@ -3,14 +3,17 @@ CREATE TABLE `actor_movie_temp` (
 	`actorOccurrence` INT(11) NOT NULL,
 	`movieTitle` VARCHAR(2000) NOT NULL,
 	`movieReleaseYear` VARCHAR(4) NOT NULL,
+	`movieOccurrence` INT(11) NOT NULL,
 	INDEX `actorName` (`actorName`),
 	INDEX `actorOccurrence` (`actorOccurrence`),
 	INDEX `FK_actor_movie_temp_movies` (`movieTitle`),
 	INDEX `FK_actor_movie_temp_movies_2` (`movieReleaseYear`),
+	INDEX `FK_actor_movie_temp_movies_3` (`movieOccurrence`),
 	CONSTRAINT `FK_actor_movie_temp_actors` FOREIGN KEY (`actorName`) REFERENCES `actors` (`name`),
 	CONSTRAINT `FK_actor_movie_temp_actors_2` FOREIGN KEY (`actorOccurrence`) REFERENCES `actors` (`occurrence`),
 	CONSTRAINT `FK_actor_movie_temp_movies` FOREIGN KEY (`movieTitle`) REFERENCES `movies` (`title`),
-	CONSTRAINT `FK_actor_movie_temp_movies_2` FOREIGN KEY (`movieReleaseYear`) REFERENCES `movies` (`releaseYear`)
+	CONSTRAINT `FK_actor_movie_temp_movies_2` FOREIGN KEY (`movieReleaseYear`) REFERENCES `movies` (`releaseYear`),
+	CONSTRAINT `FK_actor_movie_temp_movies_3` FOREIGN KEY (`movieOccurrence`) REFERENCES `movies` (`occurrence`)
 )
 COLLATE='latin1_swedish_ci'
 ENGINE=InnoDB
