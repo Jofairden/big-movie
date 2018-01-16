6d38e76af309725e7407fa4a9985d0d9843e2158
Jofairden <Jofairden@users.noreply.github.com>
2018-01-16 11:50:20 +0100

    jdbc reads sql files now

--- bot/src/main/java/bigmovie/JdbcSubroutine.java
+++ bot/src/main/java/bigmovie/JdbcSubroutine.java
@@ -1,11 +1,12 @@
 package bigmovie;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 import com.mysql.jdbc.Connection;
 import com.mysql.jdbc.Statement;
 import com.rivescript.macro.Subroutine;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,10 +19,16 @@ public class JdbcSubroutine implements Subroutine {
 
     @Override
     public String call(com.rivescript.RiveScript rs, String[] args) {
-        StringBuilder sql = new StringBuilder();
+        String sql = null;
         StringBuilder result = new StringBuilder();
-	    for (String arg : args) sql.append(" ").append(arg);
-        sql = new StringBuilder(sql.toString().trim());
+	    try {
+		    sql = Resources.toString(JdbcSubroutine.class.getResource(String.format("/sql/%s", args[0])), Charsets.UTF_8);
+	    } catch (IOException e) {
+		    e.printStackTrace();
+	    }
+
+	    if (sql == null)
+	    	return "";
 
         Connection connection = null;
         Statement statement = null;
@@ -29,12 +36,13 @@ public class JdbcSubroutine implements Subroutine {
 
         BotConfig config = Bot.getConfig();
 
+        // @todo: convert to prepared statement to avoid sql injections
         try {
             connection=(Connection) DriverManager.getConnection(
                     "jdbc:mysql://" + config.getDB_HOST() + ":" + config.getDB_PORT() + "/" + config.getDB_COL() + "?autoReconnect=true&useSSL=false",
                     config.getDB_USR(), config.getDB_PW());
             statement=(Statement) connection.createStatement();
-            resultSet=statement.executeQuery(sql.toString());
+            resultSet=statement.executeQuery(sql);
             while(resultSet.next()) {
                 int i = resultSet.getMetaData().getColumnCount();
                 for (int j = 1; j <= i; j++) {
