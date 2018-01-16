674939c7abade25062ef36f2712ad49e4a8fd43a
Jildert <jildert.venema@hotmail.com>
2018-01-16 12:09:28 +0100

    sql query in R file

--- bot/src/main/java/bigmovie/R/genre-fr-usa.R
+++ bot/src/main/java/bigmovie/R/genre-fr-usa.R
@@ -4,7 +4,13 @@ install.packages("RMySQL", repos= "http://cran.us.r-project.org")
 library(RMySQL)
 
 con <- dbConnect(MySQL(), dbname="bigmovie", user="root", password="")
-values <- dbGetQuery(con, "select title as format, id as freq from movies limit 5")
+values <- dbGetQuery(con,
+"SELECT c.country as format, count(c.genre) as freq
+FROM movies m
+INNER JOIN country_movie c ON c.movie_id=m.id
+INNER JOIN movie_genre g ON g.movie_id=m.id
+WHERE c.country LIKE \"%FRANCE%\" OR c.country LIKE \"%USA\"
+ORDER BY g.genre;")
 
 png(filename="genre-fr-usa.jpg")
 barplot(values$freq, names.arg = values$format, horiz=FALSE, cex.names=1)
--- bot/src/main/resources/sql/question10french.sql
+++ bot/src/main/resources/sql/question10french.sql
@@ -1,6 +1,6 @@
-SELECT m.title, c.country, g.genre
+SELECT c.country as format, count(c.genre) as freq
 FROM movies m
 INNER JOIN country_movie c ON c.movie_id=m.id
 INNER JOIN movie_genre g ON g.movie_id=m.id
-WHERE c.country LIKE "%FRANCE%"
+WHERE c.country LIKE "%FRANCE%" OR c.country LIKE "%USA"
 ORDER BY g.genre;
