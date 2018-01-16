8be54a4f37b55048adb9f3f2bf4be7fa93d16bca
Jofairden <Jofairden@users.noreply.github.com>
2018-01-16 11:22:29 +0100

    BotConfig

--- bot/src/main/java/bigmovie/JdbcSubroutine.java
+++ bot/src/main/java/bigmovie/JdbcSubroutine.java
@@ -18,32 +18,21 @@ public class JdbcSubroutine implements Subroutine {
 
     @Override
     public String call(com.rivescript.RiveScript rs, String[] args) {
-        String host = args[0];
-        String port = args[1];
-        String db = args[2];
-        String username = args[3];
-        String password = args[4];
         StringBuilder sql = new StringBuilder();
         StringBuilder result = new StringBuilder();
-        for (int i=5; i<args.length; i++) 
-            sql.append(" ").append(args[i]);
+	    for (String arg : args) sql.append(" ").append(arg);
         sql = new StringBuilder(sql.toString().trim());
 
         Connection connection = null;
         Statement statement = null;
         ResultSet resultSet = null;
 
-        //pak DB password uit DbPassword.txt file
-        try {
-            password = BotUtils.Readfile("src/main/java/bigmovie/DbPassword.txt", StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        BotConfig config = Bot.getConfig();
 
         try {
             connection=(Connection) DriverManager.getConnection(
-                    "jdbc:mysql://" + host + ":" + port + "/" + db + "?autoReconnect=true&useSSL=false",
-                    username, password);
+                    "jdbc:mysql://" + config.getDB_HOST() + ":" + config.getDB_PORT() + "/" + config.getDB_COL() + "?autoReconnect=true&useSSL=false",
+                    config.getDB_USR(), config.getDB_PW());
             statement=(Statement) connection.createStatement();
             resultSet=statement.executeQuery(sql.toString());
             while(resultSet.next()) {
