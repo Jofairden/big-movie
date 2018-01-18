0df72d58cf039867287a334e1a281eb826d556ad
Jildert <jildert.venema@hotmail.com>
2018-01-18 12:30:43 +0100

    top 10 language

--- bot/src/main/java/bigmovie/subroutines/PopularLanguageSubroutine.java
+++ bot/src/main/java/bigmovie/subroutines/PopularLanguageSubroutine.java
@@ -2,17 +2,19 @@ package bigmovie.subroutines;
 
 import bigmovie.Bot;
 import bigmovie.BotUtils;
+import bigmovie.PrepArg;
 import com.rivescript.macro.Subroutine;
 
-public class SoundtrackSubroutine implements Subroutine {
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
+public class PopularLanguageSubroutine implements Subroutine {
     @Override
     public String call(com.rivescript.RiveScript rs, String[] args) {
 
         String result = BotUtils.execSqlQuery(args[0], null);
 
-        result.replace("\"", "\t");
-        String msg = Bot.messageSubroutine.call(rs, String.format("context:embed fieldtitle:Soundtrack %s", result).split(" "));
+        String msg = Bot.messageSubroutine.call(rs, String.format("context:embed fieldtitle:Language %s", result).split(" "));
         BotUtils.embedErr(msg);
         return "";
     }
