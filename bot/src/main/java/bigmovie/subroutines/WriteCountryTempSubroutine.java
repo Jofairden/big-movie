4578157abd997332d375436b29eb3d03e427dc9b
Jildert <jildert.venema@hotmail.com>
2018-01-22 13:38:17 +0100

    vraag9 validatie voor argumentn

--- bot/src/main/java/bigmovie/subroutines/WriteCountryTempSubroutine.java
+++ bot/src/main/java/bigmovie/subroutines/WriteCountryTempSubroutine.java
@@ -14,15 +14,21 @@ public class WriteCountryTempSubroutine implements Subroutine{
     @Override
     public String call(com.rivescript.RiveScript rs, String[] args) {
         StringBuilder sb = new StringBuilder();
-        String path;
+        String path = null;
         if (args.length > 1){
 
-            if (args[0].equals("context:genres") && args.length > 2){
+            if (args[0].equals("context:genres") && args.length > 2 && args.length < 14){
                 path = args[1];
+
                 for (int i = 2; i < args.length; i++) {
                     sb.append(BotUtils.firstToUpper(args[i])).append("\n");
                 }
 
+                Bot.rscriptSubroutine.call(rs, new String[]{"resource:Vraag9.R"});
+                Bot.messageSubroutine.call(rs, new String[]{"context:file", "resource:vraag9.png"});
+            }
+            else if (args[0].equals("context:genres") ){
+                Bot.messageSubroutine.call(rs, new String[]{"Please add between 1 and 11 genres, splitted by space"});
             }
             else {
 
