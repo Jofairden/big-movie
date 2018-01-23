dc1b40b21ffb27c02d5d406035e48319d07202dd
Fadi <qada1600@nhl.nl>
2018-01-23 22:57:21 +0100

    youtubeV1.3
    Controlen van de film in de databse

--- bot/src/main/java/bigmovie/subroutines/YoutubeSubroutine.java
+++ bot/src/main/java/bigmovie/subroutines/YoutubeSubroutine.java
@@ -1,7 +1,9 @@
+// author Fadi
 package bigmovie.subroutines;
 
 import bigmovie.Bot;
 import bigmovie.BotUtils;
+import bigmovie.PrepArg;
 import com.rivescript.macro.Subroutine;
 
 import java.io.IOException;
@@ -10,6 +12,8 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class YoutubeSubroutine implements Subroutine {
 
@@ -17,71 +21,83 @@ public class YoutubeSubroutine implements Subroutine {
     public String call(com.rivescript.RiveScript rs, String[] args) {
 
 
-        try {
+        String sqlResult = BotUtils.execSqlQuery(args[0], new PrepArg[]{
+                new PrepArg<>(String.join("%", Arrays.stream(args).skip(1).collect(Collectors.toList())) + "%")
+        });
+        //Voer de youtube query uit
 
-            String movieName = "";
+             try {
 
-            for(int i=0; i<args.length; i++)
-            {
-                movieName = movieName + " " + args[i];
-            }
+                 float movieBool = Float.valueOf(sqlResult);
 
-            String movieTrailer = movieName + " " + "trailer";
-            String youtubeName = movieTrailer.replace(' ', '+');
-            String webPage = "https://www.youtube.com/results?sp=EgIQAQ%253D%253D&search_query=" + youtubeName;
-            //Convert de input naar een youtube link
+                 if (movieBool == 1.0) {
+                    // Controleer of de film in de database zit
 
-            URL url = new URL(webPage);
-            URLConnection urlConnection = url.openConnection();
-            InputStream is = urlConnection.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is);
-            //Maak de connectie naar de youtube link aan
+                     String movieName = "";
 
-            int numCharsRead;
-            char[] charArray = new char[1024];
-            StringBuffer sb = new StringBuffer();
-            while ((numCharsRead = isr.read(charArray)) > 0) {
-                sb.append(charArray, 0, numCharsRead);
-            }
-            String result = sb.toString();
-            //Zet de inhoud van de youtube link in een string
+                     for (int i = 1; i < args.length; i++) {
+                         movieName = movieName + " " + args[i];
+                     }
+                     //Bouw de volledige naam van de film
 
-            String search = "<a href=\"/watch?v=";
-            int indexFound = result.indexOf(search);
-            int indexStart = indexFound + search.length();
-            int indexEnd = 0;
-            int indexCounter = indexStart;
-            String stringEnd = "\"";
+                     String movieTrailer = movieName + " " + "trailer";
+                     String youtubeName = movieTrailer.replace(' ', '+');
+                     String webPage = "https://www.youtube.com/results?sp=EgIQAQ%253D%253D&search_query=" + youtubeName;
+                     //Convert de input naar een youtube link
+
+                     URL url = new URL(webPage);
+                     URLConnection urlConnection = url.openConnection();
+                     InputStream is = urlConnection.getInputStream();
+                     InputStreamReader isr = new InputStreamReader(is);
+                     //Maak de connectie naar de youtube link aan
+
+                     int numCharsRead;
+                     char[] charArray = new char[1024];
+                     StringBuffer sb = new StringBuffer();
+                     while ((numCharsRead = isr.read(charArray)) > 0) {
+                         sb.append(charArray, 0, numCharsRead);
+                     }
+                     String result = sb.toString();
+                     //Zet de inhoud van de youtube link in een string
+
+                     String search = "<a href=\"/watch?v=";
+                     int indexFound = result.indexOf(search);
+                     int indexStart = indexFound + search.length();
+                     int indexEnd = 0;
+                     int indexCounter = indexStart;
+                     String stringEnd = "\"";
 
 
-            while (!result.substring(indexCounter,indexCounter+1).equals(stringEnd))
-            {
-                indexCounter += 1;
+                     while (!result.substring(indexCounter, indexCounter + 1).equals(stringEnd)) {
+                         indexCounter += 1;
 
-                if(result.substring(indexCounter,indexCounter+1).equals(stringEnd))
-                {
-                    indexEnd = indexCounter;
-                }
-            }
-            //Zoek waar de substring in de string met de website inhoud zit en sla de begin en eid positie ervan op
+                         if (result.substring(indexCounter, indexCounter + 1).equals(stringEnd)) {
+                             indexEnd = indexCounter;
+                         }
+                     }
+                     //Zoek waar de substring in de string met de website inhoud zit en sla de begin en eid positie ervan op
 
-            String webAdres = "https://www.youtube.com/watch?v=" + result.substring(indexStart,indexEnd);
-            System.out.println(movieName);
-            System.out.println(webPage);
-            System.out.println(indexStart);
-            System.out.println(indexEnd);
-            System.out.println(movieName);
-            System.out.println("https://www.youtube.com/watch?v=" + result.substring(indexStart,indexEnd));
-            //Maak de link aan van de meest relevante video
+                     String webAdres = "https://www.youtube.com/watch?v=" + result.substring(indexStart, indexEnd);
+                     System.out.println(movieName);
+                     System.out.println(webPage);
+                     System.out.println(indexStart);
+                     System.out.println(indexEnd);
+                     System.out.println(movieName);
+                     System.out.println("https://www.youtube.com/watch?v=" + result.substring(indexStart, indexEnd));
+                     //Maak de link aan van de meest relevante video
 
-            return webAdres;
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+                     return webAdres;
+                 }
+                 else{
+                     return "Deze film bestaat niet";
+                 }
+             } catch (MalformedURLException e) {
+                 e.printStackTrace();
+             } catch (IOException e) {
+                 e.printStackTrace();
+             } catch(NumberFormatException ex){
+        System.err.println("Er is iets fout gegaan");
+    }
         return "";
     }
 
